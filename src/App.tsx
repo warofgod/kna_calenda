d07f6450abe90ba39abbcc7e32853b7da60d9871
sonmunwoo <warofgodsna@naver.com>
2026-05-25 15:18:05 +0900

    수정

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ function App() {
     <main className="app">
       <h1>skn_calenda</h1>
       <p>Vercel 호스팅 확인 — 캘린더 일정관리 프로젝트</p>
+      <input type="text"  />
+      <button>추가</button>
+      
     </main>
   )
 }
